b7e0204f934053e704a56d0e29b7b8e98c1c3a98
ftayebee <fahim.tayebee@gmail.com>
2026-04-11 01:14:30 +0600

    API URL CHECK

--- src/components/api/config.js
+++ src/components/api/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-    window.location.href.startsWith("https://madcricketers.com") || window.location.href.startsWith("https://madcricketers.vercel.app/")
+    window.location.href.startsWith("https://madcricketers.com") || window.location.href.startsWith("https://www.madcricketers.com") || window.location.href.startsWith("https://madcricketers.vercel.app/")
         ? "https://app.madcricketers.com/api"
         : "http://127.0.0.1:8000/api";
 
